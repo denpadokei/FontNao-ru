18c58a23986be85e49069bf0ca1b9e0c52e13388
denpadokei <jsoldenpa@yahoo.co.jp>
2023-11-10 23:48:27 +0900

    プロジェクト ファイルを追加します。

---
+++ FontNao-ru/Models/FontLoader.cs
@@ -0,0 +1,147 @@
+﻿using BeatSaberMarkupLanguage;
+using FontNao_ru.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
+using Zenject;
+
+
+namespace FontNao_ru.Models
+{
+    internal static class FontLoader
+    {
+        private static Material s_noGlow;
+        public static Material UINoGlowMaterial => s_noGlow ?? (s_noGlow = Resources.FindObjectsOfTypeAll<Material>().Where(m => m.name == "UINoGlow").FirstOrDefault());
+
+        private static Shader s_tmpNoGlowFontShader;
+        public static Shader TMPNoGlowFontShader => s_tmpNoGlowFontShader ?? (s_tmpNoGlowFontShader = !BeatSaberUI.MainTextFont ? null : BeatSaberUI.MainTextFont.material.shader);
+
+        // DaNike to the rescue 
+        public static bool TryGetTMPFontByFamily(string family, out TMP_FontAsset font)
+        {
+            if (FontManager.TryGetTMPFontByFamily(family, out font)) {
+                font.material.shader = TMPNoGlowFontShader;
+                return true;
+            }
+
+            return false;
+        }
+        private static readonly string FontAssetPath = Path.Combine(Environment.CurrentDirectory, "UserData", "FontAssets");
+        private static readonly string MainFontPath = Path.Combine(FontAssetPath, "Main");
+        private static readonly string FallBackFontPath = Path.Combine(FontAssetPath, "FallBack");
+
+        public static bool IsInitialized { get; private set; } = false;
+
+        private static TMP_FontAsset _mainFont = null;
+
+        public static TMP_FontAsset MainFont
+        {
+            get
+            {
+                if (!_mainFont) {
+                    return null;
+                }
+                if (_mainFont.material.shader != TMPNoGlowFontShader) {
+                    _mainFont.material.shader = TMPNoGlowFontShader;
+                }
+                return _mainFont;
+            }
+            private set => _mainFont = value;
+        }
+
+        private static List<TMP_FontAsset> _fallbackFonts = new List<TMP_FontAsset>();
+        public static List<TMP_FontAsset> FallBackFonts
+        {
+            get
+            {
+                foreach (var font in _fallbackFonts) {
+                    if (font.material.shader != TMPNoGlowFontShader) {
+                        font.material.shader = TMPNoGlowFontShader;
+                    }
+                }
+                return _fallbackFonts;
+            }
+            private set => _fallbackFonts = value;
+        }
+
+        
+        public static async Task CreateChatFont()
+        {
+            IsInitialized = false;
+            while (TMPNoGlowFontShader == null) {
+                await Task.Delay(10);
+            }
+            if (MainFont != null) {
+                GameObject.Destroy(MainFont);
+            }
+            foreach (var font in FallBackFonts) {
+                if (font != null) {
+                    GameObject.Destroy(font);
+                }
+            }
+            if (!Directory.Exists(MainFontPath)) {
+                _ = Directory.CreateDirectory(MainFontPath);
+            }
+            if (!Directory.Exists(FallBackFontPath)) {
+                _ = Directory.CreateDirectory(FallBackFontPath);
+            }
+
+            
+            TMP_FontAsset asset = null;
+            AssetBundle bundle = null;
+            foreach (var filename in Directory.EnumerateFiles(MainFontPath, "*.assets", SearchOption.TopDirectoryOnly)) {
+                using (var fs = File.OpenRead(filename)) {
+                    bundle = AssetBundle.LoadFromStream(fs);
+                }
+                if (bundle != null) {
+                    break;
+                }
+            }
+            if (bundle != null) {
+                foreach (var bundleItem in bundle.GetAllAssetNames()) {
+                    asset = bundle.LoadAsset<TMP_FontAsset>(Path.GetFileNameWithoutExtension(bundleItem));
+                    if (asset != null) {
+                        MainFont = asset;
+                        bundle.Unload(false);
+                        break;
+                    }
+                }
+            }
+
+            _fallbackFonts.Clear();
+            foreach (var fallbackFontPath in Directory.EnumerateFiles(FallBackFontPath, "*.assets")) {
+                using (var fs = File.OpenRead(fallbackFontPath)) {
+                    bundle = AssetBundle.LoadFromStream(fs);
+                }
+                if (bundle == null) {
+                    continue;
+                }
+                foreach (var bundleItem in bundle.GetAllAssetNames()) {
+                    asset = bundle.LoadAsset<TMP_FontAsset>(Path.GetFileNameWithoutExtension(bundleItem));
+                    if (asset != null) {
+                        _fallbackFonts.Add(asset);
+                    }
+                }
+                bundle.Unload(false);
+            }
+            foreach (var osFontPath in Font.GetPathsToOSFonts()) {
+                if (Path.GetFileNameWithoutExtension(osFontPath).ToLower() != "meiryo") {
+                    continue;
+                }
+                var meiryo = new Font(osFontPath)
+                {
+                    name = Path.GetFileNameWithoutExtension(osFontPath)
+                };
+                asset = TMP_FontAsset.CreateFontAsset(meiryo);
+                _fallbackFonts.Add(asset);
+            }
+            IsInitialized = true;
+        }
+    }
+}
---
+++ FontNao-ru/Patch/LoadAndSetUpFontFallbacksAsyncPatch.cs
@@ -0,0 +1,30 @@
+﻿using BeatSaberMarkupLanguage;
+using FontNao_ru.Models;
+using HarmonyLib;
+using IPA.Utilities.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontNao_ru.Patch
+{
+    [HarmonyPatch(nameof(BeatSaberMarkupLanguage.Plugin.OnStart))]
+    internal class LoadAndSetUpFontFallbacksAsyncPatch
+    {
+        [HarmonyPostfix]
+        public static async void Postfix()
+        {
+            await UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
+            {
+                await FontLoader.CreateChatFont();
+                var mainfont = BeatSaberUI.MainTextFont;
+                var tmp = FontLoader.FallBackFonts.ToList();
+                tmp.AddRange(mainfont.fallbackFontAssetTable.ToList());
+                mainfont.fallbackFontAssetTable.Clear();
+                mainfont.fallbackFontAssetTable.AddRange(tmp);
+            });
+        }
+    }
+}
