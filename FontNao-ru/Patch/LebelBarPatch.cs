335c796e1224354d70275f619e6375f11b6dae3f
denpadokei <jsoldenpa@yahoo.co.jp>
2023-11-12 00:09:42 +0900

    曲タイトルが長いと曲時間が読めなくなる不具合の修正

--- FontNao-ru/Patch/LebelBarPatch.cs
+++ FontNao-ru/Patch/LebelBarPatch.cs
@@ -10,8 +10,11 @@ namespace FontNao_ru.Patch
         [HarmonyPrefix]
         public static void HarmonyPrefix(ref TextMeshProUGUI ____songNameText)
         {
+            ____songNameText.enableAutoSizing = true;
+            ____songNameText.fontSizeMax = 4f;
+            ____songNameText.fontSizeMin = 2.5f;
             ____songNameText.enableWordWrapping = false;
-            ____songNameText.overflowMode = TextOverflowModes.Overflow;
+            ____songNameText.overflowMode = TextOverflowModes.Ellipsis;
         }
     }
 }
