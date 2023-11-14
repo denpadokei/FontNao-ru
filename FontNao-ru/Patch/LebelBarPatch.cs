cd6f0b03931adff8979f9c6877ba902927b7a5d5
denpadokei <jsoldenpa@yahoo.co.jp>
2023-11-14 22:14:48 +0900

    一部BSMLで作成したリストで文字化けしていたのを対応

--- FontNao-ru/Patch/LebelBarPatch.cs
+++ FontNao-ru/Patch/LebelBarPatch.cs
@@ -1,24 +1,47 @@
 ﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Components;
 using FontNao_ru.Models;
 using HarmonyLib;
+using HMUI;
 using System.Linq;
 using TMPro;
 using UnityEngine;
 
 namespace FontNao_ru.Patch
 {
+    /// <summary>
+    /// メインの曲リストのみ適応
+    /// </summary>
     [HarmonyPatch(typeof(LevelListTableCell))]
-    [HarmonyPatch(nameof(LevelListTableCell.SetDataFromLevelAsync), 0)]
-    internal class LebelBarPatch
+    [HarmonyPatch(nameof(LevelListTableCell.SetDataFromLevelAsync), MethodType.Normal)]
+    internal class LevelListTableCellSetDataFromLevelAsyncPatch
+    {
+
+        [HarmonyPrefix]
+        public static void HarmonyPrefix(ref TextMeshProUGUI ____songNameText)
+        {
+            if (____songNameText) {
+                ____songNameText.enableAutoSizing = true;
+                ____songNameText.fontSizeMax = 4f;
+                ____songNameText.fontSizeMin = 2.5f;
+            }
+        }
+    }
+
+    /// <summary>
+    /// BSMLのリストでバグる
+    /// </summary>
+    [HarmonyPatch(typeof(LevelListTableCell))]
+    [HarmonyPatch(nameof(LevelListTableCell.enabled), MethodType.Setter)]
+    internal class LevelListTableCellEnablePatch
     {
         [HarmonyPrefix]
         public static void HarmonyPrefix(ref TextMeshProUGUI ____songNameText)
         {
-            ____songNameText.enableAutoSizing = true;
-            ____songNameText.fontSizeMax = 4f;
-            ____songNameText.fontSizeMin = 2.5f;
-            ____songNameText.enableWordWrapping = false;
-            ____songNameText.overflowMode = TextOverflowModes.Ellipsis;
+            if (____songNameText) {
+                ____songNameText.enableWordWrapping = false;
+                ____songNameText.overflowMode = TextOverflowModes.Ellipsis;
+            }
         }
     }
 }
