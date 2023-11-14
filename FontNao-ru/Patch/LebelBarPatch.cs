a33548ebc43e740618650ca46920eef0a4e1284b
denpadokei <jsoldenpa@yahoo.co.jp>
2023-11-14 15:45:37 +0900

    全フォントを変更する暴挙を実装

--- FontNao-ru/Patch/LebelBarPatch.cs
+++ FontNao-ru/Patch/LebelBarPatch.cs
@@ -14,9 +14,6 @@ namespace FontNao_ru.Patch
         [HarmonyPrefix]
         public static void HarmonyPrefix(ref TextMeshProUGUI ____songNameText)
         {
-            
-            ____songNameText.font = FontLoader.MainFont;
-
             ____songNameText.enableAutoSizing = true;
             ____songNameText.fontSizeMax = 4f;
             ____songNameText.fontSizeMin = 2.5f;
